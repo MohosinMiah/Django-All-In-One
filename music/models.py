95980853aacf284ca57f88a3b82deba9695fd0df
mohosin16103308 <hamza1610330816@gmail.com>
2019-08-15 09:16:09 +0600

    Create Two Table in Music App

--- music/models.py
+++ music/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Album(models.Model):
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=255)
+    genere = models.CharField(max_length=255)
+    album_logo = models.CharField(max_length=255)
+    pub_date = models.DateTimeField('date published')
+
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=200)
+    song_title =models.CharField(max_length=255)
--- music/views.py
+++ music/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the Music App  index.")
+
+
+def favorite(request):
+    return HttpResponse("Hello, world. You're at the Music/Favorite App  index.")
+
+

ba22bc51de624c7b6554696a8aed2bd5e0712711
mohosin16103308 <hamza1610330816@gmail.com>
2019-08-16 08:48:04 +0600

    Adding Song in Adminpanel

--- music/models.py
+++ music/models.py
@@ -7,9 +7,13 @@ class Album(models.Model):
     genere = models.CharField(max_length=255)
     album_logo = models.CharField(max_length=255)
     pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.album_title +' - '+self.artist
 
 
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=200)
     song_title =models.CharField(max_length=255)
+    def __str__(self):
+        return self.song_title + '  - Album Name : '+self.album.album_title
--- music/views.py
+++ music/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 from .models import Album
@@ -7,14 +7,14 @@ from .models import Album
 
 def index(request):
     album_list = Album.objects.all()
-    # template = loader.get_template('music/index.html')
+    template = loader.get_template('music/index.html')
     context = {
         'album_list': album_list,
     }
-    return render(request, 'index.html', {'album_list': album_list})
+    return render(request, 'music/index.html', {'album_list': album_list})
 def favorite(request):
     return HttpResponse("Hello, world. You're at the Music/Favorite App  index.")
 
 def detail(request, album_id):
-    return HttpResponse("You're looking at question %s." % album_id)
-
+    album_details = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album_details': album_details})
